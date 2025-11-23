46a6778e865d3cd61170beda36395479abb327b5
devpankov <mikhailpankov166@gmail.com.email>
2025-11-23 17:46:03 +0300

    Single file implementation

--- main.cpp
+++ main.cpp
@@ -4,62 +4,52 @@
 #include <algorithm>
 #include <cstdio>
 
-#define LOG(fmt, ...) printf(fmt, __VA_ARGS__)
+#define DBG_PRINT(fmt, ...) printf(fmt, __VA_ARGS__)
 
-int g_counter = 0;
+int G_ALLOC_COUNT = 0;
 
-class MyMem : public std::pmr::memory_resource {
-    struct block_t {
-        void* p;
-        size_t sz;
+class Lab17Resource : public std::pmr::memory_resource {
+    struct _chunk_info {
+        void* addr;
+        size_t len;
     };
 
-    std::vector<block_t> cache;
-    std::vector<void*> sys;
+    std::vector<_chunk_info> _pool;
+    std::vector<void*> _sys;
 
 public:
-    ~MyMem() {
-        for (size_t i = 0; i < sys.size(); ++i) {
-            ::operator delete(sys[i]);
+    ~Lab17Resource() {
+        for (auto p : _sys) { 
+            ::operator delete(p); 
         }
-        LOG(">> Dead. Total: %d\n", g_counter);
+        DBG_PRINT(">> Resource died. Total allocs: %d\n", G_ALLOC_COUNT);
     }
 
 protected:
-    void* do_allocate(size_t bytes, size_t align) override {
-        g_counter++;
+    void* do_allocate(size_t bytes, size_t alignment) override {
+        G_ALLOC_COUNT++;
         
-        int idx = -1;
+        for (int i = 0; i < (int)_pool.size(); i++) {
+            if (_pool[i].len == bytes) {
+                void* ptr = _pool[i].addr;
+                
+                _pool[i] = _pool.back();
+                _pool.pop_back();
 
-        for (int i = 0; i < (int)cache.size(); ++i) {
-            if (cache[i].sz == bytes) {
-                idx = i;
-                break;
+                DBG_PRINT("[MEM] Reuse old block: %p (%zu b)\n", ptr, bytes);
+                return ptr;
             }
         }
 
-        if (idx != -1) {
-            void* ret = cache[idx].p;
-            
-            cache[idx] = cache.back();
-            cache.pop_back();
-
-            LOG("[MEM] Reuse: %p (%zu)\n", ret, bytes);
-            return ret;
-        }
-
-        void* ptr = ::operator new(bytes, std::align_val_t(align));
-        sys.push_back(ptr);
-        LOG("[MEM] New: %p (%zu)\n", ptr, bytes);
+        void* ptr = ::operator new(bytes, std::align_val_t(alignment));
+        _sys.push_back(ptr);
+        DBG_PRINT("[MEM] System new: %p (%zu b)\n", ptr, bytes);
         return ptr;
     }
 
-    void do_deallocate(void* p, size_t bytes, size_t align) override {
-        block_t b;
-        b.p = p;
-        b.sz = bytes;
-        cache.push_back(b);
-        LOG("[MEM] Free: %p\n", p);
+    void do_deallocate(void* p, size_t bytes, size_t alignment) override {
+        _pool.push_back({p, bytes});
+        DBG_PRINT("[MEM] Return to pool: %p\n", p);
     }
 
     bool do_is_equal(const std::pmr::memory_resource& other) const noexcept override {
@@ -68,94 +58,96 @@ protected:
 };
 
 template <typename T>
-class Stk {
+class SimpleStack {
+private:
     struct Node {
-        T val;
+        T data;
         Node* next;
     };
 
-    std::pmr::polymorphic_allocator<Node> al;
-    Node* top;
+    std::pmr::polymorphic_allocator<Node> _allocator;
+    Node* _head;
 
 public:
-    Stk(std::pmr::memory_resource* r) : al(r) {
-        top = 0;
-    }
+    SimpleStack(std::pmr::memory_resource* mr) 
+        : _allocator(mr), _head(nullptr) 
+    {}
 
-    ~Stk() {
-        while (top != 0) {
+    ~SimpleStack() {
+        while (_head) {
             pop();
         }
     }
 
-    void push(const T& v) {
-        Node* n = al.allocate(1);
-        al.construct(n, Node{v, top});
-        top = n;
+    void push(const T& val) {
+        Node* n = _allocator.allocate(1);
+        _allocator.construct(n, Node{val, _head});
+        _head = n;
     }
 
     void pop() {
-        if (top == 0) return;
+        if (!_head) return;
         
-        Node* tmp = top;
-        top = top->next;
+        Node* tmp = _head;
+        _head = _head->next;
         
-        al.destroy(tmp);
-        al.deallocate(tmp, 1);
+        _allocator.destroy(tmp);
+        _allocator.deallocate(tmp, 1);
     }
 
-    class It {
-    public:
+    struct MyIter {
         using iterator_category = std::forward_iterator_tag;
         using value_type = T;
         using difference_type = std::ptrdiff_t;
         using pointer = T*;
         using reference = T&;
 
-        Node* cur;
-        It(Node* c) : cur(c) {}
+        Node* curr;
+        MyIter(Node* c) : curr(c) {}
 
-        T& operator*() { return cur->val; }
-        It& operator++() { 
-            if(cur) cur = cur->next; 
+        T& operator*() { return curr->data; }
+        MyIter& operator++() { 
+            if(curr) curr = curr->next; 
             return *this; 
         }
-        bool operator!=(const It& o) { return cur != o.cur; }
+        bool operator!=(const MyIter& o) { return curr != o.curr; }
     };
 
-    It begin() { return It(top); }
-    It end() { return It(0); }
+    MyIter begin() { return MyIter(_head); }
+    MyIter end() { return MyIter(nullptr); }
 };
 
-struct Data {
-    int x;
-    float y;
+struct MyStruct {
+    int id;
+    float val;
 };
 
 int main() {
-    MyMem m;
+    Lab17Resource res;
 
-    std::cout << "--- INT ---\n";
+    std::cout << "--- TEST 1 (INT) ---\n";
     {
-        Stk<int> s(&m);
-        s.push(1);
-        s.push(2);
+        SimpleStack<int> s(&res);
+        s.push(10);
+        s.push(20);
         
-        for(auto x : s) std::cout << x << " ";
+        for(auto x : s) {
+            std::cout << x << " ";
+        }
         std::cout << "\n";
 
         s.pop(); 
         s.pop();
 
-        std::cout << "--- CHECK ---\n";
-        s.push(3);
+        std::cout << "Pushing new item (expect reuse)...\n";
+        s.push(999);
     }
 
-    std::cout << "\n--- DATA ---\n";
+    std::cout << "\n--- TEST 2 (STRUCT) ---\n";
     {
-        Stk<Data> sd(&m);
-        sd.push({10, 5.5});
-        std::cout << sd.begin().cur->val.y << "\n";
+        SimpleStack<MyStruct> s2(&res);
+        s2.push({1, 3.14f});
+        std::cout << "Top struct val: " << s2.begin().curr->data.val << "\n";
     }
 
     return 0;
